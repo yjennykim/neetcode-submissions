2d58e3419eb62f0dc16cf41fffa8c355e0a5b10d
jenny <77416265+yjennykim@users.noreply.github.com>
2026-06-13 18:09:19 -0700

    Add: find-k-closest-elements - submission-11

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-11.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        l, r = 0, len(arr)-1
+        while l < r:
+            m = (l + r + 1) // 2
+            if arr[m] <= x:
+                l = m 
+            else:
+                r = m - 1
+        
+        # Binary search finds the biggest L < X
+        # But L+1 could be closer than the number bigger
+        if l < len(arr)-1 and abs(x - (arr[l+1])) < abs(x - (arr[l])):
+            l += 1
+        r = l
+        while r-l+1 < k:
+            if r == len(arr) - 1:
+                l -= 1
+            elif l == 0:
+                r += 1
+            elif abs(arr[r+1] - x) < abs(arr[l-1] - x):
+                r += 1
+            else:
+                l -= 1
+        
+        return arr[l:r+1]
